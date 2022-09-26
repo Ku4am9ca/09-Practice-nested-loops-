7a63ddedc04e445e957fc4519b830b8ca3a083b7
Viktor <tsukanov87@mail.ru>
2022-09-26 21:31:30 +0300

    new commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let out = document.querySelector(".out");
+console.log(out);
+
+
+//for (let i = 0; i < 10; i++) {
+//    //i =0
+//    for (let k = 0; k < 10; k++) {
+//        //k = 0
+//        out.innerHTML += "*";
+//    }
+//    out.innerHTML += "<br>"
+//}
+
+for (let i = 1; i < 10; i++) {
+    //out.innerHTML += "3*" + i + "=" + (i * 3) + "<br>";
+    for (let k = 1; k < 10; k++) {
+        out.innerHTML += `${i}*${k}=${k * i}<br>`;
+    }
+    out.innerHTML += "<hr>";
+}
+
